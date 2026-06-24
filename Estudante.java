bc18330a6489a115facb46cde06a658e6e6cbe46
Silvana <silvana.braatz65@gmail.com>
2026-06-23 21:01:30 -0300

    Criando a classe Boletim

---
+++ Estudante.java
@@ -0,0 +1,20 @@
+public class Estudante {
+    public String nome;
+    public int matricula;
+
+    public Estudante(String nome, int matricula){
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+// Getters
+public String getNome() {
+    return nome;
+    }
+
+public int getMatricula(){
+    return matricula;
+    }
+}
+
+
